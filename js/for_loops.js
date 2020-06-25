7f9d1598f1d84782e03a3f161d955f2b0d86b0f3
Tommy Cirilo-Thorpe <toperman@Tommys-iMac.local>
2020-06-25 11:26:51 -0500

    working on exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,4 +1,15 @@
 function showMultiplicationTable (num){
+    return num;
 
 }
-console.log(showMultiplicationTable(5));
+
+console.log("7 * 1 is " + (showMultiplicationTable(7 * 1)));
+console.log("7 * 2 is " + (showMultiplicationTable(7 * 2)));
+console.log("7 * 3 is " + (showMultiplicationTable(7 * 3)));
+console.log("7 * 4 is " + (showMultiplicationTable(7 * 4)));
+console.log("7 * 5 is " + (showMultiplicationTable(7 * 5)));
+console.log("7 * 6 is " + (showMultiplicationTable(7 * 6)));
+console.log("7 * 7 is " + (showMultiplicationTable(7 * 7)));
+console.log("7 * 8 is " + (showMultiplicationTable(7 * 8)));
+console.log("7 * 9 is " + (showMultiplicationTable(7 * 9)));
+console.log("7 * 10 is " + (showMultiplicationTable(7 * 10)));
