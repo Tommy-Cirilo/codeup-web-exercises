44f333f19959c47a28cc44a6e0056bb212c57ab9
Tommy Cirilo-Thorpe <toperman@Tommys-iMac.local>
2020-06-25 15:29:33 -0500

    working on the first for loop

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,15 +1,20 @@
 function showMultiplicationTable (num){
     return num;
-
 }
+console.log("7 x 1 is " + (showMultiplicationTable(7 * 1)));
+console.log("7 x 2 is " + (showMultiplicationTable(7 * 2)));
+console.log("7 x 3 is " + (showMultiplicationTable(7 * 3)));
+console.log("7 x 4 is " + (showMultiplicationTable(7 * 4)));
+console.log("7 x 5 is " + (showMultiplicationTable(7 * 5)));
+console.log("7 x 6 is " + (showMultiplicationTable(7 * 6)));
+console.log("7 x 7 is " + (showMultiplicationTable(7 * 7)));
+console.log("7 x 8 is " + (showMultiplicationTable(7 * 8)));
+console.log("7 x 9 is " + (showMultiplicationTable(7 * 9)));
+console.log("7 x 10 is " + (showMultiplicationTable(7 * 10)));
 
-console.log("7 * 1 is " + (showMultiplicationTable(7 * 1)));
-console.log("7 * 2 is " + (showMultiplicationTable(7 * 2)));
-console.log("7 * 3 is " + (showMultiplicationTable(7 * 3)));
-console.log("7 * 4 is " + (showMultiplicationTable(7 * 4)));
-console.log("7 * 5 is " + (showMultiplicationTable(7 * 5)));
-console.log("7 * 6 is " + (showMultiplicationTable(7 * 6)));
-console.log("7 * 7 is " + (showMultiplicationTable(7 * 7)));
-console.log("7 * 8 is " + (showMultiplicationTable(7 * 8)));
-console.log("7 * 9 is " + (showMultiplicationTable(7 * 9)));
-console.log("7 * 10 is " + (showMultiplicationTable(7 * 10)));
+/*function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}*/
+
