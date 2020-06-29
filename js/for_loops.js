2fde4c84b62f9b1125fbe2f4223d4ee9566e327e
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-06-29 14:53:27 -0500

    update

--- js/for_loops.js
+++ js/for_loops.js
@@ -9,7 +9,7 @@ while (num <= 10) {
     num ++;
     console.log("7 x " + num + " is " + (7 * num));
 }
-console.log("we are done");
+console.log("finished first problem");
 
 //alternate instructor solution
 /*
@@ -27,4 +27,21 @@ console.log(multiply(4, 5));*/
 /*Use a for loop and the code from the previous lessons to
 generate 10 random numbers between 20 and 200 and
 output to the console whether each number is odd or even. For example:*/
+
+let even = " is even";
+let isOdd = " is odd";
+
+for (let i=0; i <10 ; i++) {
+    //for iteration purposes
+    // console.log(i);
+    let num = Math.floor(Math.random() * 181 ) + 20;
+    if (num %2===0) {
+        console.log("the number " + num + even);
+    } else{
+        console.log("the number " + num + isOdd);
+    }
+}
+console.log("finished second problem");
+
+
 
