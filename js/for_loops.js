2254fa4bd69592ca3ec266c4aa359eb48cd20e8a
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-06-29 12:03:37 -0500

    update

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,20 +1,31 @@
-function showMultiplicationTable (num){
-    return num;
-}
-console.log("7 x 1 is " + (showMultiplicationTable(7 * 1)));
-console.log("7 x 2 is " + (showMultiplicationTable(7 * 2)));
-console.log("7 x 3 is " + (showMultiplicationTable(7 * 3)));
-console.log("7 x 4 is " + (showMultiplicationTable(7 * 4)));
-console.log("7 x 5 is " + (showMultiplicationTable(7 * 5)));
-console.log("7 x 6 is " + (showMultiplicationTable(7 * 6)));
-console.log("7 x 7 is " + (showMultiplicationTable(7 * 7)));
-console.log("7 x 8 is " + (showMultiplicationTable(7 * 8)));
-console.log("7 x 9 is " + (showMultiplicationTable(7 * 9)));
-console.log("7 x 10 is " + (showMultiplicationTable(7 * 10)));
 
-/*function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}*/
+/*Create a function named showMultiplicationTable that accepts a number and
+console.logs the multiplication table
+for that number (just multiply by the numbers 1 through 10)*/
+
+let num = 0;
+
+while (num <= 10) {
+    num ++;
+    console.log("7 x " + num + " is " + (7 * num));
+}
+console.log("we are done");
+
+//alternate instructor solution
+/*
+let multiply = function(a, b) {
+    let answer = 0;
+    for (let i = 0; i < b; i++) {
+        answer += a;
+    }
+    return answer;
+}
+
+console.log(multiply(4, 5));*/
+
+
+/*Use a for loop and the code from the previous lessons to
+generate 10 random numbers between 20 and 200 and
+output to the console whether each number is odd or even. For example:*/
+
 
