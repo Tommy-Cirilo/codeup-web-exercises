f1d2db4200afc8d369ef8805a1fbfc5b5b2853c5
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-06-30 10:23:08 -0500

    update

--- js/for_loops.js
+++ js/for_loops.js
@@ -62,3 +62,32 @@ for (m = 1; m <= count; m++) {
         console.log(""+m+" ");
     }
 }*/
+
+/*Create a for loop that uses console.log to create the output shown below.
+
+
+100
+95
+90
+85
+80
+75
+70
+65
+60
+55
+50
+45
+40
+35
+30
+25
+20
+15
+10
+5*/
+
+for (let i=100; i>=5; i-5){
+    console.log(i);
+}
+console.log("fourth problem is done");
