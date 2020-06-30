7eff916edcc456222bd32cdcd40968fb2b898941
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-06-30 14:01:24 -0500

    new

---
+++ js/while.js
@@ -0,0 +1,32 @@
+/*While Loops
+Create a file named while.js in the js directory.
+
+    Create a while loop that uses console.log() to create the output shown below.
+
+
+2
+4
+8
+16
+32
+64
+128
+256
+512
+1024
+2048
+4096
+8192
+16384
+32768
+65536*/
+
+
+let num=2;
+
+while (num < 65536) {
+    num *=2;
+    console.log(num);
+}
+
+
