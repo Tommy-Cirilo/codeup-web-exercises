9d2d10b749b0dcd0025b3bc5bc13c94474cd0c94
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-07-02 16:53:17 -0500

    new exercises

---
+++ js/planets-array.js
@@ -0,0 +1,43 @@
+(function(){
+    "use strict";
+
+    var planets = [
+        'Mercury',
+        'Venus',
+        'Earth',
+        'Mars',
+        'Jupiter',
+        'Saturn',
+        'Uranus',
+        'Neptune'
+    ];
+
+    /**
+     * TODO:
+     * Read each console log below, and write some javascript code to perform
+     * the step that it describes
+     */
+
+    console.log('Adding "The Sun" to the beginning of the planets array.');
+    console.log(planets.unshift("The sun"))
+    console.log(planets);
+
+    console.log('Adding "Pluto" to the end of the planets array.');
+    console.log(planets.push("pluto"));
+    console.log(planets);
+
+    console.log('Removing "The Sun" from the beginning of the planets array.');
+    console.log(planets.shift());
+    console.log(planets);
+
+    console.log('Removing "Pluto" from the end of the planets array.');
+    console.log(planets.pop());
+
+    console.log('Finding and logging the index of "Earth" in the planets array.');
+    console.log("the index of earth is: " + planets.indexOf("Earth"));
+    console.log("Reversing the order of the planets array.");
+    console.log(planets.reverse());
+
+    console.log("Sorting the planets array.");
+    console.log(planets.sort());
+})();
