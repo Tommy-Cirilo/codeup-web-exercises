6b5b2726c0a36a1682f11e8153cdc77f25a3f27e
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-07-02 15:04:37 -0500

    update

--- js/manipulate_arrays.js
+++ js/manipulate_arrays.js
@@ -1,9 +1,51 @@
 let weekdays = ["monday" , "tuesday" , "wednesday", "thursday" , "friday"];
 
-console.log(weekdays.length);
 
 
-console.log();
+console.log("the days array length is: " + weekdays.length);
+
+
+weekdays.unshift("sunday");
+weekdays.push ("saturday");
+console.log("The weekdays array data: ");
+
+weekdays.forEach(function (day) {
+    console.log('\t', day);
+})
+
+
+
+
+let array = [1,2,3,4,5];
+
+console.log(array);
+
+let removeditem = array.pop();
+
+console.log("removed last item: " + removeditem);
+
+let removeitem = array.shift();
+
+console.log("removed first item" + removeitem);
+
+console.log(array);
+
+
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'red'];
+
+console.log(colors[5]);
+
+
+
+let fruits = ["banana", "orange", "apple", "mango"];
+
+let slicedfruit = fruits.slice(1, 3);
+
+
+console.log(slicedfruit);
+
+
+
 
 
 
