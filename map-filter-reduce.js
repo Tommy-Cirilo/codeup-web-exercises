253ee770496114a618c1eeb426b4aec66c6cd147
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-07-30 16:54:50 -0500

    update

---
+++ map-filter-reduce.js
@@ -0,0 +1,61 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+
+
+
+//Use .filter to create an array of user objects where each user object has at least 3 languages
+// in the languages array.
+
+let user = users.filter(lang => lang.languages.length >= 3);
+
+console.log(user);
+
+
+
+
+
+//Use .map to create an array of strings where each element is a user's email address
+
+var useremail = users.map(emails => emails.email);
+
+console.log(useremail);
+
+//Use .reduce to get the total years of experience from the list of users. Once you get the
+// total of years you can use the result to calculate the average.
+
