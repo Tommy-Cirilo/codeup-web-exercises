813468d2abbf940ff416738c58e2779e9236e789
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-07-31 10:20:04 -0500

    update

--- map-filter-reduce.js
+++ map-filter-reduce.js
@@ -52,10 +52,77 @@ console.log(user);
 
 //Use .map to create an array of strings where each element is a user's email address
 
-var useremail = users.map(emails => emails.email);
+let useremail = users.map(emails => emails.email);
 
 console.log(useremail);
 
+
+
 //Use .reduce to get the total years of experience from the list of users. Once you get the
 // total of years you can use the result to calculate the average.
+
+let totalyears = users.reduce((i, user)=> i + user.yearsOfExperience,0);
+
+
+console.log(totalyears / users.length);
+
+
+
+//Use .reduce to get the longest email from the list of users.
+
+let longestEmail = users.reduce((longemail, emails) => {
+    if (emails.email.length > longemail.length) {
+        return user.email;
+    } else {
+        return longemail;
+    }
+}, "");
+
+
+
+//Use .reduce to get the list of user's names in a single string. Example: Your
+// instructors are: ryan, luis, zach, fernando, justin.
+
+
+
+/*Bonus
+Use .reduce to get the unique list of languages from the list of users.
+
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];*/
 
