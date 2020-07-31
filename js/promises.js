29675a9f09371ba60d685519ecaf9dd24440d515
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-07-31 15:44:13 -0500

    update

--- js/promises.js
+++ js/promises.js
@@ -2,16 +2,26 @@ const myPromise = fetch('https://api.github.com/users/tommy-cirilo/events/public
 
 
 
-myPromise.then(response => console.log(response.json().then(function (data) {
+myPromise.then(response => console.log(response.json()
+    .then(function (data) {
     console.log(data);
 
 
+    console.log(data[0].actor.login);
 
+    console.log(data[0].created_at)
+/*
+    function user(username) {
+        console.log(data[0].created_at)
+    }*/
 
-
-
-
+    // user(Tommy-Cirilo);
 })));
+
+
+
+
+
 myPromise.catch(error => console.error(error)); 
 
 
