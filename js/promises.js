1a0ae244e0fd3c70c718684cdeb1f5bba966cc66
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-07-31 14:09:15 -0500

    update

--- js/promises.js
+++ js/promises.js
@@ -1,1 +1,19 @@
-fetch(url, {headers: {'Authorization': 'token YOUR_TOKEN_HERE'}})
+const myPromise = fetch('https://api.github.com/users/tommy-cirilo/events/public', {headers: {"Authorization": `gitHubApiKey`}});
+
+
+
+myPromise.then(response => console.log(response.json().then(function (data) {
+    console.log(data);
+
+
+
+
+
+
+
+})));
+myPromise.catch(error => console.error(error)); 
+
+
+
+
