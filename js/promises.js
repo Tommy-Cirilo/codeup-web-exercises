dfe41eac969a09bc6bab04961c9937283a8a1218
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-07-31 16:55:45 -0500

    update

--- js/promises.js
+++ js/promises.js
@@ -1,22 +1,17 @@
 const myPromise = fetch('https://api.github.com/users/tommy-cirilo/events/public', {headers: {"Authorization": `gitHubApiKey`}});
 
-
+/*Create a function that accepts a GitHub username, and returns a promise that resolves returning just the date of
+the last commit that user made. Reference the github api documentation to achieve this*/
 
 myPromise.then(response => console.log(response.json()
     .then(function (data) {
     console.log(data);
-
-
-    console.log(data[0].actor.login);
-
-    console.log(data[0].created_at)
-/*
-    function user(username) {
-        console.log(data[0].created_at)
-    }*/
-
-    // user(Tommy-Cirilo);
+    console.log(data[0].actor.login); //brings back username
+    console.log(data[0].created_at)   // brings back time
 })));
+
+
+
 
 
 
