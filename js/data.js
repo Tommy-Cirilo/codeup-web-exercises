4ac81a24598cace6932b3d292bc92f03181722e0
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-08-03 09:45:15 -0500

    update

---
+++ js/data.js
@@ -0,0 +1,17 @@
+//create an object that can be used by exporting to another file
+
+
+const data{
+    theAnswer: 42,
+        whichSideOfTheForce: "the dark side",
+        scarn: "cri-man-saqa, fnc , double time!"
+}
+
+
+
+
+
+
+
+
+
